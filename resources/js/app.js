5fae7f0f4044522878f0b9e64d4b38bae121bf93
Sarvar <sarvar_web@texnomart.uz>
2022-10-07 17:06:13 +0500

    test

---
+++ resources/js/app.js
@@ -0,0 +1,27 @@
+require('./bootstrap');
+
+let onlineUsers = 0;
+
+function update_online_counter() {
+    document.getElementById('online').textContent = '' + onlineUsers;
+}
+
+window.Echo.join('common_room')
+    .here((users) => {
+        onlineUsers = users.length;
+
+        update_online_counter();
+    })
+    .joining((user) => {
+        onlineUsers++;
+
+        update_online_counter();
+    })
+    .leaving((user) => {
+        onlineUsers--;
+
+        update_online_counter();
+    })
+    listen('NewMessage', (e) => {
+        console.log(e)
+    });
